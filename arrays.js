63b502a338893ac1da0d6e32b0dd1bb3d8e0ccb7
Anh Nguyen <anhnguyenis@gmail.com>
2019-05-30 21:52:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,19 +1,10 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 
 function addElementToBeginningOfArray(array,element) {
-  return [element,...array];
+  return array.unshift(element);
 }
 
 function addElementToEndOfArray(array, element) {
   return [...array, element];
 }
 
-function destructivelyAddElementToEndOfArray(array, element) {
- array.push(element);
- return array
-}
-
-function destructivelyAddElementToEndOfArray(array, element) {
- array.unshift(element);
- return array
-}
